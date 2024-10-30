a654502398582ee667bf1a7ccca750beaae65b8e
mohashafici <mohashafici44@gmail.com>
2024-10-30 11:18:07 +0300

    Implement getTask endpoint to retrieve and send tasks as JSON response

--- controllers/tasksController.js
+++ controllers/tasksController.js
@@ -1,16 +1,36 @@
-
-//exports ma aanan sameynin
-
+const {IncomingForm}=require('formidable');
 const { readTaskFromFile, writeTasksTOFile } = require("../utils/fileHandler")
 
 exports.getTask = (req,res)=>{
  const tasks=readTaskFromFile()
+ res.writeHead(200 ,{ 'Content-Type': 'application/json' });
+ res.end(JSON.stringify(tasks));
 }
 
-exports.createTask = (req,res)=>{
-    const  tasks=writeTasksTOFile(req,res)
+// exports.createTask = (req,res)=>{
+const form=new IncomingForm();
+form.parse(req,(err,fields,files)=>{
 
-}
+    if(err){
+        res.writeHead(400,{'Content-Type':'application/json'});
+        res.end(JSON.stringify({message:'error parsing form'}));
+        return;
+    }
+    const tasks=readTaskFromFile()
+    const newTask={
+        id:Date.now(),
+        title:fields.title,
+        description:fields.description,
+        status:fields?.status ||  'pending',
+        image:fields.image ?`/uploads/${fields.image.name}`:null,
+    }
+    tasks.push(newTask)
+
+    writeTasksTOFile(tasks);
+
+
+})
+// }
 exports.updatedTask = (req,res)=>{}
 
 exports.deleteTask = (req,res)=>{}

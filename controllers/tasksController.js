8aae81d9245296d82298feb91cf90a644d0bfaab
mohashafici <mohashafici44@gmail.com>
2024-10-30 10:41:25 +0300

    Implement CRUD operations for tasks in tasksController.js

--- controllers/tasksController.js
+++ controllers/tasksController.js
@@ -1,14 +1,15 @@
 
 //exports ma aanan sameynin
 
-const { readTaskFromFile } = require("../utils/fileHandler")
+const { readTaskFromFile, writeTasksTOFile } = require("../utils/fileHandler")
 
 exports.getTask = (req,res)=>{
  const tasks=readTaskFromFile()
 }
 
 exports.createTask = (req,res)=>{
-    
+    const  tasks=writeTasksTOFile(req,res)
+
 }
 exports.updatedTask = (req,res)=>{}
 

07a02755113dd3b9be9bc5f32ff21e9d935cac84
mohashafici <mohashafici44@gmail.com>
2024-11-05 12:00:33 +0300

    all changes

--- utils/fileHandler.js
+++ utils/fileHandler.js
@@ -5,14 +5,12 @@ const filePath = path.join(__dirname, '../data/tasks.json');
 
 // Write tasks to file
 exports.writeTasksToFile = (tasks) => {
-    fs.writeFileSync(filePath, JSON.stringify(tasks,null, 2));   // modifying to json file:1 commit message
+    fs.writeFileSync(filePath, JSON.stringify(tasks,null, 2));   
 };
 
-// Read tasks from file
 exports.readTaskFromFile = () => {
-    // If file does not exist, initialize it with an empty array
     if (!fs.existsSync(filePath)) {
-        this.writeTasksToFile([]);  // Use `exports.writeTasksToFile` directly
+        this.writeTasksToFile([]);  
     }
     const data = fs.readFileSync(filePath);
 
